99e76dfd001b59fe39f876fee924e5a36e3b5c2c
Wybe Westra <wybe@ruurdwestra.nl>
2025-02-22 09:58:36 +0100

    Basic eframe app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use eframe::egui::{CentralPanel, Context, Grid};
+
 fn main() {
-    println!("Hello, world!");
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "Spinners",
+        native_options,
+        Box::new(|cc| Ok(Box::new(SpinnerApp::new(cc)))),
+    )
+    .unwrap();
+}
+
+#[derive(Default)]
+struct SpinnerApp {}
+
+impl SpinnerApp {
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        cc.egui_ctx.style_mut(|style| style.visuals.striped = true);
+
+        Self::default()
+    }
+}
+
+impl eframe::App for SpinnerApp {
+    fn update(&mut self, ctx: &Context, _frame: &mut eframe::Frame) {
+        CentralPanel::default().show(ctx, |ui| {
+            Grid::new("Spinner grid").num_columns(2).show(ui, |ui| {
+                ui.label("Default");
+                ui.spinner();
+                ui.end_row();
+            });
+        });
+    }
 }
